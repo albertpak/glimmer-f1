3920fbe0fe6998a033befb8c1700b354d5ecc9c9
Nick Steffens <nsteffens@gmail.com>
2017-04-12 20:05:14 -0500

    Adding pagination

--- src/ui/components/drivers-list/component.ts
+++ src/ui/components/drivers-list/component.ts
@@ -2,16 +2,39 @@ import Component, { tracked } from '@glimmer/component';
 
 export default class DriversList extends Component {
   @tracked drivers;
+  @tracked limit;
+  @tracked total;
+  @tracked offset;
+  @tracked currentPage = 0;
 
-  constructor(options) {
-    super(options)
+  didInsertElement() {
     this.loadInitDrivers();
   }
 
   async loadInitDrivers() {
     let response = await fetch('http://ergast.com/api/f1/drivers.json');
     let json = await response.json();
-
+    this.limit = json.MRData.limit;
+    this.offset = json.MRData.offset;
+    this.total = json.MRData.total;
     this.drivers = json.MRData.DriverTable.Drivers;
+  }
+
+  async pagination(page) {
+    this.currentPage = this.currentPage + page;
+    this.offset = this.limit * this.currentPage;
+    if ( this.offset < this.total && this.offset >= 0 ) {
+      let response = await fetch('http://ergast.com/api/f1/drivers.json?offset=' + parseInt(this.offset));
+      let json = await response.json();
+      this.drivers = json.MRData.DriverTable.Drivers;
+    }
+  }
+
+  next() {
+    this.pagination(1);
+  }
+
+  prev() {
+    this.pagination(-1);
   }
 };
